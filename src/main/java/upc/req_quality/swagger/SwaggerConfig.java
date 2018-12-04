8f4e8075f973f150c33998a6a1b984ff7f826ec6
FerranA <fewrna@gmail.com>
2018-12-04 21:51:58 +0100

    Improved swagger documentation

--- src/main/java/upc/req_quality/swagger/SwaggerConfig.java
+++ src/main/java/upc/req_quality/swagger/SwaggerConfig.java
@@ -17,33 +17,36 @@ public class SwaggerConfig {
     private static final String SWAGGER_API_VERSION = "0.2";
     private static final String LICENCE_TEXT = "License";
     private static final String title = "Requirement Conformance to Templates";
-    private static final String description = "This service is meant to provide automation for checking requirements" +
+    private static final String description = "<p> This service is meant to provide automation for checking requirements" +
             " conformance to boilerplates/templates. A boilerplate or template organizes the syntactic structure of a" +
-            " requirement statement into a number of pre-defined slots.\n" +
-            "\n" +
-            " This API allows the writing of templates following a defined format. Those templates are stored in an" +
+            " requirement statement into a number of pre-defined slots.</p>" +
+            "" +
+            " <p> This API allows the writing of templates following a defined format. Those templates are stored in an" +
             " internal database distributed according to the organization that created them. Then you can check if a" +
             " set of requirements follows the structure of the previous templates. For doing so, the requirements are" +
             " first converted to include their part-of-speech and sentence" +
-            " tags with a library that can be specified.\n" +
-            "\n" +
-            " Before writing a new template you should choose a NLP library of those permitted by this service because" +
-            " each one has different tags. A template follows a BNF modified diagram:\n" +
-            " -A template is defined by one or more rules (the input is defined by an array of strings).\n" +
-            " -The first word of each rule must be written as ```#<name_of_the_rule> ::=``` that defines the name of the rule.\n" +
-            " -The name of the first rule must be main.\n" +
-            " -The first rule must define the structure of the requirement.\n" +
-            " -The other rules should be used to define auxiliary structures.\n" +
-            " -The auxiliary rules must be in incremental order.\n" +
-            " -For writing rules are permitted the next tags: words (specified with \"%\"), pos tags of the template's library (specified with \"()\"), sentence tags of the template's library (specified with \"<>\") and service tags.\n\n" +
-            " An example:\n" +
-            "```\n" +
-            "main rule --> #<main> ::=  <opt-condition> <np> (md) (vb) <np> | <opt-condition> <np> <modal> %PROVIDE <np> %WITH %THE %ABILITY <infinitive-vp> <np> | <opt-condition> <np> <modal> %BE %ABLE <vp> <np>\n" +
-            "aux rule -->  #<conditional-keyword> ::= %IF | %AFTER | %AS %SOON %AS | %AS %LONG %AS\n" +
-            "aux rule -->  #<modal-algo> ::= %SHALL | %SHOULD | %WOULD\n" +
-            "aux rule -->  #<opt-condition> ::= <conditional-keyword> | (all)\n" +
-            "aux rule -->  #<infinitive-vp> ::= %to <vp> \n```\n" +
-            "";
+            " tags with a library that can be specified.</p>" +
+            "" +
+            " <p>Before writing a new template you should choose a NLP library of those permitted by this service because" +
+            " each one has different tags. A template follows a BNF modified diagram:</p>" +
+            " <ul>" +
+            " <li>A template is defined by one or more rules (the input is defined by an array of strings).</li>" +
+            " <li>The first word of each rule must be written as \" #&ltname_of_the_rule&gt ::= \" that defines the name of the rule.</li>" +
+            " <li>The name of the first rule must be main.</li>" +
+            " <li>The first rule must define the structure of the requirement.</li>" +
+            " <li>The other rules should be used to define auxiliary structures.</li>" +
+            " <li>The auxiliary rules must be in incremental order.</li>" +
+            " <li>For writing rules are permitted the next tags (their meaning can be seen in the get clauses method): <ul>" +
+            " <li> words (specified with \"%\"). </li>" +
+            " <li> pos tags of the template's library (specified with \"()\"). </li>" +
+            " <li> sentence tags of the template's library (specified with \"<>\"). </li>" +
+            " <li> service special tags:  \"|\" , (all)  and <*>.</li></ul></li></ul>" +
+            "<p> An example: </p> <ul> " +
+            "<li> #&ltmain&gt ::= &ltopt-condition&gt &ltnp&gt (md) (vb) &ltnp&gt | &ltopt-condition&gt &ltnp&gt &ltmodal&gt %PROVIDE &ltnp&gt %WITH %THE %ABILITY &ltinfinitive-vp&gt &ltnp&gt | &ltopt-condition&gt &ltnp&gt &ltmodal&gt %BE %ABLE &ltvp&gt &ltnp&gt </li>" +
+            "<li> #&ltconditional-keyword&gt ::= %IF | %AFTER | %AS %SOON %AS | %AS %LONG %AS </li>" +
+            "<li> #&ltmodal&gt ::= %SHALL | %SHOULD | %WOULD </li>" +
+            "<li> #&ltopt-condition&gt ::= &ltconditional-keyword&gt | (all) </li>" +
+            "<li> #&ltinfinitive-vp&gt ::= %to &ltvp&gt </li> </ul>";
 
 
     /*
@@ -73,7 +76,7 @@ public class SwaggerConfig {
                 .title(title)
                 .description(description)
                 .license(LICENCE_TEXT)
-                .license(SWAGGER_API_VERSION)
+                //.license(SWAGGER_API_VERSION)
                 .build();
     }
 
