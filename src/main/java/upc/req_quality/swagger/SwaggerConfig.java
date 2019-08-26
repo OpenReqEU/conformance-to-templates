46a5457e28980dabcf4676fc9a58c1c4613b09a0
Ferran Agulló López <fewrnap@gmail.com>
2019-08-26 16:19:45 +0200

    may solve the bug with swagger

--- src/main/java/upc/req_quality/swagger/SwaggerConfig.java
+++ src/main/java/upc/req_quality/swagger/SwaggerConfig.java
@@ -1,17 +1,22 @@
 package upc.req_quality.swagger;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import upc.req_quality.RestApiController;
 
 import java.util.HashSet;
 
 @Configuration
+@PropertySource("classpath:swagger.properties")
+@ComponentScan(basePackageClasses = RestApiController.class)
 @EnableSwagger2
 public class SwaggerConfig {
     private static final String TITLE = "Requirements Conformance to Templates";
